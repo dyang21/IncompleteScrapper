4f39dc55a837c111a29142aab1deba7c8d7953d9
dyang21 <dyang32@binghamton.edu>
2019-08-24 13:08:16 -0400

    Initial commit

---
+++ AmazonApp.py
@@ -0,0 +1,28 @@
+# Allows me to get data from URL; BS4 is for parsing and pulling specific data
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "https://www.amazon.de/Pillars-Eternity-II-Deadfire-Obsidian-PC/dp/B07999KF6W"
+
+# Allows me to get information from my browser
+
+headers = {
+    "User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+                 }
+
+
+page = requests.get(URL, headers=headers)
+
+
+soup1 = BeautifulSoup(page.content, 'html.parser')
+soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
+
+
+# Pull information out using f12
+title = soup2.find(id= "productTitle")
+
+print(title)
+
+# mission failed. make the html code in javascript.
